c681c3a544a82a15a792ada6e1806727c17be7d1
BlueZore <kally32@163.com>
2014-08-22 16:18:30 +0800

    1

---
+++ OSSClient/OSSClient/UserControl1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Aliyun.OpenServices;
+using Aliyun.OpenServices.OpenStorageService;
+using System.IO;
+using System.Threading;
+
+namespace OSSClient
+{
+    [Guid("614e92d3-e688-4af0-bd2d-d86aa83d86f2")]
+    public partial class UserControl1 : UserControl
+    {
+
+        static string endPoint = "http://oss-cn-beijing.aliyuncs.com";
+        static string accessKeyID = "yNwTNYUewtVW4hFl";
+        static string accessKeySecret = "sQx9gDGpM9Tep76CuSAafYafQQxe8k";
+        static string bucketName = "aliyunoss001";
+        static string key = "";
+
+        BackgroundWorker bw = new BackgroundWorker();
+        private ManualResetEvent manualReset = new ManualResetEvent(true);
+        delegate void SetTextCallback(string text);
+
+        static string path = "";
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpLoad_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox.Items.Add(new ListItem(openFileDialog1.FileName, "0"));
+                listBox.DisplayMember = "Name";
+                listBox.ValueMember = "Value";
+
+                //mutiPartUpload();
+            }
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        { // 这被工作线程调用
+            mutiPartUpload();
+        }
+
+        public void mutiPartUpload()
+        {
+            for (; ; )
+            {
+                getListItemName();
+
+                if (string.IsNullOrEmpty(path))
+                {
+                    break;
+                }
+
+                key = Guid.NewGuid().ToString() + path.Substring(path.LastIndexOf('.'));
+
+                OssClient ossClient = new OssClient(endPoint, accessKeyID, accessKeySecret);
+
+                InitiateMultipartUploadRequest initRequest =
+                                new InitiateMultipartUploadRequest(bucketName, key);
+                InitiateMultipartUploadResult initResult = ossClient.InitiateMultipartUpload(initRequest);
+
+
+                // 设置每块为 5M ,不允许小于5M
+                int partSize = 1024 * 100;
+
+                FileInfo partFile = new FileInfo(path);
+
+                // 计算分块数目 
+                int partCount = (int)(partFile.Length / partSize);
+                if (partFile.Length % partSize != 0)
+                {
+                    partCount++;
+                }
+
+                // 新建一个List保存每个分块上传后的ETag和PartNumber 
+                List<PartETag> partETags = new List<PartETag>();
+
+                for (int i = 0; i < partCount; i++)
+                {
+                    Bar.Value = (i * 100) / partCount;
+                    UploadInfo(Bar.Value.ToString());
+
+                    // 获取文件流 
+                    FileStream fis = new FileStream(partFile.FullName, FileMode.Open);
+
+                    // 跳到每个分块的开头 
+                    long skipBytes = partSize * i;
+                    fis.Position = skipBytes;
+                    //fis.skip(skipBytes); 
+
+                    // 计算每个分块的大小 
+                    long size = partSize < partFile.Length - skipBytes ?
+                            partSize : partFile.Length - skipBytes;
+
+                    // 创建UploadPartRequest，上传分块 
+                    UploadPartRequest uploadPartRequest = new UploadPartRequest(bucketName, key, initResult.UploadId);
+                    uploadPartRequest.InputStream = fis;
+                    uploadPartRequest.PartSize = size;
+                    uploadPartRequest.PartNumber = (i + 1);
+                    UploadPartResult uploadPartResult = ossClient.UploadPart(uploadPartRequest);
+
+                    // 将返回的PartETag保存到List中。 
+                    partETags.Add(uploadPartResult.PartETag);
+
+                    // 关闭文件 
+                    fis.Close();
+
+                    manualReset.WaitOne();//如果ManualResetEvent的初始化为终止状态（true），那么该方法将一直工作，直到收到Reset信号。然后，直到收到Set信号，就继续工作。
+                }
+
+                CompleteMultipartUploadRequest completeReq = new CompleteMultipartUploadRequest(bucketName, key, initResult.UploadId);
+                foreach (PartETag partETag in partETags)
+                {
+                    completeReq.PartETags.Add(partETag);
+                }
+                //  红色标注的是与JAVA的SDK有区别的地方 
+
+                //完成分块上传 
+                CompleteMultipartUploadResult completeResult = ossClient.CompleteMultipartUpload(completeReq);
+
+                Bar.Value = 100;
+                UploadInfo(Bar.Value.ToString());
+
+                // 返回最终文件的MD5，用于用户进行校验 
+                //Console.WriteLine(completeResult.ETag);
+
+                setListItemValue();
+            }
+        }
+
+        private void btPause_Click(object sender, EventArgs e)
+        {
+            if (btPause.Text == "暂停")
+            {
+                manualReset.Reset();//暂停当前线程的工作，发信号给waitOne方法，阻塞
+                btPause.Text = "继续";
+            }
+            else
+            {
+                manualReset.Set();//继续某个线程的工作
+                btPause.Text = "暂停";
+            }
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            bw.CancelAsync();
+        }
+
+        void UploadInfo(string text)
+        {
+            if ("100" == text)
+            {
+                SetText("'" + path + "'文件，已完成");
+            }
+            else
+            {
+                SetText("'" + path + "'文件，已上传" + text + "%");
+            }
+        }
+
+        // 此方法演示一个对windows窗体控件作线程安全调用的模式
+        // 如果调用线程和创建TextBox控件的线程不同，这个方法创建
+        // 代理SetTextCallback并且自己通过Invoke方法异步调用它
+        // 如果相同则直接设置Text属性
+        private void SetText(string text)
+        {
+            // InvokeRequired需要比较调用线程ID和创建线程ID
+            // 如果它们不相同则返回true
+            if (this.lbInfo.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lbInfo.Text = text;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bw.DoWork += bw_DoWork;
+            bw.RunWorkerAsync("");
+        }
+
+        void getListItemName()
+        {
+            path = "";
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                ListItem item = listBox.Items[i] as ListItem;
+                if (item.Value == "0")
+                {
+                    path = item.Name;
+                    break;
+                }
+            }
+        }
+
+        void setListItemValue()
+        {
+            List<ListItem> list = new List<ListItem>();
+            bool getValue = false;
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                ListItem item = listBox.Items[i] as ListItem;
+                if (item.Value == "0" && !getValue)
+                {
+                    getValue = true;
+                    item.Value = "1";
+                    item.Name += "[完成]";
+                }
+                list.Add(item);
+            }
+
+            listBox.DisplayMember = "Name";
+            listBox.ValueMember = "Value";
+            listBox.DataSource = list;
+        }
+    }
+
+    public class ListItem
+    {
+        public ListItem(string Name, string Value)
+        {
+            this.Name = Name;
+            this.Value = Value;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ OSSClient/WinApp/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Bar = new System.Windows.Forms.ProgressBar();
+            this.btPause = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUpLoad
+            // 
+            this.btnUpLoad.BackColor = System.Drawing.SystemColors.Control;
+            this.btnUpLoad.Location = new System.Drawing.Point(12, 319);
+            this.btnUpLoad.Name = "btnUpLoad";
+            this.btnUpLoad.Size = new System.Drawing.Size(60, 25);
+            this.btnUpLoad.TabIndex = 0;
+            this.btnUpLoad.Text = "上传";
+            this.btnUpLoad.UseVisualStyleBackColor = false;
+            this.btnUpLoad.Click += new System.EventHandler(this.btnUpLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Bar
+            // 
+            this.Bar.Location = new System.Drawing.Point(12, 15);
+            this.Bar.Name = "Bar";
+            this.Bar.Size = new System.Drawing.Size(417, 27);
+            this.Bar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.Bar.TabIndex = 1;
+            // 
+            // btPause
+            // 
+            this.btPause.BackColor = System.Drawing.SystemColors.Control;
+            this.btPause.Location = new System.Drawing.Point(144, 319);
+            this.btPause.Name = "btPause";
+            this.btPause.Size = new System.Drawing.Size(60, 25);
+            this.btPause.TabIndex = 3;
+            this.btPause.Text = "暂停";
+            this.btPause.UseVisualStyleBackColor = false;
+            this.btPause.Click += new System.EventHandler(this.btPause_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.BackColor = System.Drawing.SystemColors.Control;
+            this.btCancel.Location = new System.Drawing.Point(210, 319);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(60, 25);
+            this.btCancel.TabIndex = 4;
+            this.btCancel.Text = "取消";
+            this.btCancel.UseVisualStyleBackColor = false;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lbInfo.Location = new System.Drawing.Point(14, 54);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(0, 12);
+            this.lbInfo.TabIndex = 5;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(12, 76);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(417, 232);
+            this.listBox.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.Control;
+            this.btnStart.Location = new System.Drawing.Point(78, 319);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(60, 25);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 354);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btPause);
+            this.Controls.Add(this.Bar);
+            this.Controls.Add(this.btnUpLoad);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar Bar;
+        private System.Windows.Forms.Button btPause;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
