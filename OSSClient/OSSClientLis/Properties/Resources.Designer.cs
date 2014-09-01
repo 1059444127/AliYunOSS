b1abe9fc9f3a2c198379093dff1faa71013084f7
BlueZore <kally32@163.com>
2014-09-01 11:12:41 +0800

    3

--- OSSClient/OSSClientLis/Properties/Resources.Designer.cs
+++ OSSClient/OSSClientLis/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace OSSClientLis.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap close {
+            get {
+                object obj = ResourceManager.GetObject("close", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap play {
             get {
                 object obj = ResourceManager.GetObject("play", resourceCulture);
